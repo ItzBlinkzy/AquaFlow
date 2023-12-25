5c2dffffe740befccd1d42cea5742f9c2fcbe982
Kevin <kitey13579@gmail.com>
2023-12-25 11:38:13 +0000

    fix: setting secure cookies over https, allowing certain methods for an endpoint

--- serverless/api/users/sign-out.ts
+++ serverless/api/users/sign-out.ts
@@ -2,6 +2,12 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 import clearJwtToken from "../../utils/clearJwtToken";
 
 export default async function SignOut(req: VercelRequest, res: VercelResponse) {
+  if (req.method !== "GET") {
+    res
+      .status(405)
+      .json({ status: res.statusCode, message: "Method not allowed." });
+    return;
+  }
   await clearJwtToken(req, res);
   res.json({ status: res.statusCode, message: "Successfully signed out." });
 }
